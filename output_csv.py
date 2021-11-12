b18ebb313ff9c4753c04a508480fef710c8462f1
KimSY6250 <89988290+KimSY6250@users.noreply.github.com>
2021-11-12 14:20:09 +0900

    Add files via upload

---
+++ output_csv.py
@@ -0,0 +1,34 @@
+import csv
+import os
+import time
+
+
+class CSVExport:
+
+    def __init__(self, name):
+        self.file_name = name
+        self.file_pointer = None
+
+    def input_dict(self, file_list):
+        self.__open_file()
+
+        column = file_list[0]
+        wr = csv.DictWriter(self.file_pointer, fieldnames=column, delimiter='\t')
+        wr.writeheader()
+        for i in range(1, len(file_list)):
+            tmp={}
+            for row in range(len(column)):
+                tmp[column[row]]=file_list[i][row]
+            wr.writerow(tmp)
+
+        self.__close_file()
+
+    def __open_file(self):
+        f = self.file_name + str(time.time()) + ".csv"
+        filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), f)
+        self.file_pointer = open(filename, 'w', newline='', encoding='UTF-16')
+
+    def __close_file(self):
+        self.file_pointer.close()
+
+
