29a3cba0801a09d4283a53bb759e3181c3e11376
Yeseong Hwang <37142520+yerssong@users.noreply.github.com>
2021-11-12 15:04:01 +0900

    Create gdrive_build.ps1

---
+++ gdrive_build.ps1
@@ -0,0 +1,11 @@
+# Project-AF Cloud
+# version 211112 by YES
+
+python -m venv ./venv
+.\venv\Scripts\activate
+
+.\venv\Scripts\pip install --upgrade pip
+.\venv\Scripts\pip install termcolor
+.\venv\Scripts\pip install pyfiglet
+.\venv\Scripts\pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
