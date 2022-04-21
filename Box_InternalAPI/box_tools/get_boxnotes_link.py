da1546a7f9d9fba96f1728eed456af946d520a11
winner324 <79895530+winner324@users.noreply.github.com>
2022-04-21 23:32:06 +0900

    Update get_boxnotes_link.py

--- Box_InternalAPI/box_tools/get_boxnotes_link.py
+++ Box_InternalAPI/box_tools/get_boxnotes_link.py
@@ -1,15 +1,27 @@
 import requests
 import json
+import re
 
 class notes_link:
-    def __init__(self, z, file_id):
+    def __init__(self, z, request_token,  file_id):
         self.z = z
         self.file_id = file_id
+        self.request_token = request_token
 
     def f_link(self):
         cookies = {
             'z': self.z,
         }
+        data = {
+            'request_token': self.request_token,
+        }
         url = "https://app.box.com/app-api/enduserapp/item/" + self.file_id+"/shared-link"
-        res = requests.post(url='https://app.box.com/index.php',cookies=cookies, verify=False)
-        print(res.content)
+        res = requests.post(url,cookies=cookies, data =data, verify=False)
+        # print(res.content)
+        split_data = "\"sharedLink\":(.+?),\"type\""
+        string = re.search(split_data,str(res.content)).group(1)
+        # print(string)
+        
+        
+        return string
+
