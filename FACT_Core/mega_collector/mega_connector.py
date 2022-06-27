efcc2759044bcd4a885a787cd393ca8ebd222ee0
KimSY6250 <89988290+KimSY6250@users.noreply.github.com>
2022-06-27 14:23:48 +0900

    Add files via upload

--- FACT_Core/mega_collector/mega_connector.py
+++ FACT_Core/mega_collector/mega_connector.py
@@ -52,8 +52,26 @@ class MEGA_connector:
                         print("Please put correct number.")
                         continue
                     c.download_file(download_number)
-            elif menu == 3:  # search thumbnail
-                pass
+            elif menu == 3:  # search
+                sm = cd.search_menu()
+                s_input = CInput()
+                if sm == 0:
+                    continue
+                elif sm == 1:
+                    q = input("What do you want to search for? >> ")
+                    c.search_file(q)
+                elif sm == 2:
+                    search_period = s_input.set_m_period()
+                    if search_period[0] and search_period[1]:
+                        if search_period[0] != '1970-01-01':
+                            re_start_time = datetime.datetime.strptime(search_period[0],
+                                                                       "%Y-%m-%d") - datetime.timedelta(days=1)
+                            re_start_time = re_start_time.strftime("%Y-%m-%d")
+                        else:
+                            re_start_time = search_period[0]
+                        s_period = re_start_time
+                        e_period = search_period[1]
+                        c.search_file_by_date(s_period, e_period)
             else:
                 print(" [!] Invalid Menu. Choose Again.")
                 continue
